6d618fdbb16b845f92397e5d13432dbd31d9ab24
andrey73m <andrey12mora@gmail.com>
2024-04-18 21:39:50 -0500

    Se obtiene municipio por medio del codigo de departamento

--- routes/domicilio.js
+++ routes/domicilio.js
@@ -1,8 +1,20 @@
 import { Router } from "express"
 import { colombiaAPIController } from "../controllers/colombia_api.js";
+import { municipioParamValidator } from "../validators/munipicio_validator.js";
+import { checkValidator } from "../middlewares.js";
 
 //TODO:IMPORTANTE >  Obtener municipio segun codigo departamento, codigo que pase por el body o parametro de la URL
 
 export const domicilioRouter = Router()
 
 domicilioRouter.get("/listaDepartamentos", colombiaAPIController.obtenerDepartamentos);
+
+domicilioRouter.get("/listaMunicipios/:c_departamento", 
+    // municipioParamValidator,
+    // checkValidator,
+    colombiaAPIController.obtenerMunicipiosPorDep
+);
+
+domicilioRouter.post("/direccion",
+
+);
