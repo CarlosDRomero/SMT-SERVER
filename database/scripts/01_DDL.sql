fb87d3256a7145a7df343c3688a2a0c78fab38b6
CarlosDRomero <cdavid.romeror@gmail.com>
2024-04-19 23:17:19 -0500

    creando tablas para manejo de productos y archivos csv para datos iniciales

--- database/scripts/01_DDL.sql
+++ database/scripts/01_DDL.sql
@@ -1,3 +1,5 @@
+/* INICIAL DEL SISTEMA DE USUARIOS */
+
 CREATE TYPE rolesUsuario AS ENUM ('admin', 'empleado', 'cliente');
 
 CREATE TABLE usuario (
@@ -41,6 +43,65 @@ CREATE TABLE codigo_verificacion(
   ON DELETE CASCADE
 );
 
+/* 
+ * 
+ * 
+ * INICIAL DEL SISTEMA DE COMPONENTES COMO PRODUCTO 
+ * 
+ * 
+ * */
+
+CREATE TABLE atributo_espec(
+	idespec serial PRIMARY KEY,
+	atributo varchar(150) UNIQUE
+);
+
+CREATE TABLE categoria_componente(
+	idcategoria serial PRIMARY KEY,
+	denominacion varchar(30) UNIQUE
+);
+
+CREATE TABLE categoria_espec(
+	idcat_espec serial PRIMARY KEY,
+	idespec integer NOT NULL,
+	idcategoria integer NOT NULL,
+	
+	CONSTRAINT espec_categoria FOREIGN KEY (idespec) REFERENCES atributo_espec(idespec)  ON DELETE CASCADE,
+	CONSTRAINT categoria_espec FOREIGN KEY (idcategoria) REFERENCES categoria_componente(idcategoria)
+);
+
+CREATE TABLE componente(
+	idcomponente uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	idcategoria integer NOT NULL,
+	marca varchar(20) NOT NULL,
+	nombre varchar(50) NOT NULL,
+	descripcion varchar(1000) NOT NULL,
+	url_imagen varchar(500) NOT NULL,
+	
+	CONSTRAINT categoria_componente FOREIGN KEY(idcategoria) REFERENCES categoria_componente(idcategoria)
+	
+);
+
+CREATE TABLE componente_espec(
+	idcat_espec integer NOT NULL,
+	idcomponente uuid NOT NULL,
+	valor varchar(100),
+	
+	PRIMARY KEY (idcat_espec, idcomponente),
+	CONSTRAINT cat_espec_componente FOREIGN KEY (idcat_espec) REFERENCES categoria_espec(idcat_espec)  ON DELETE CASCADE,
+	CONSTRAINT componente_cat_espec FOREIGN KEY (idcomponente) REFERENCES componente(idcomponente) ON DELETE CASCADE
+);
+
+CREATE TABLE inventario(
+	idproducto uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	idcomponente uuid NOT NULL,
+	SKU varchar(22) UNIQUE,
+	disponibilidad integer,
+	precio float,
+	
+	CONSTRAINT unique_sku_componente UNIQUE(idcomponente, SKU),
+	CONSTRAINT inventario_componente FOREIGN KEY (idcomponente) REFERENCES componente(idcomponente) ON DELETE CASCADE
+);
 
 CREATE VIEW vista_clientes AS SELECT * FROM usuario WHERE rol='cliente';
 CREATE VIEW vista_empleados AS SELECT * FROM usuario WHERE rol='empleado';
---
+++ database/scripts/04_DML.sql
@@ -0,0 +1,29 @@
+COPY atributo_espec(idespec, atributo)
+FROM '/docker-entrypoint-initdb.d/initial_data/DBCC(especificacion).csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY categoria_componente(idcategoria, denominacion)
+FROM '/docker-entrypoint-initdb.d/initial_data/DBCC(categoria).csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY categoria_espec(idcat_espec, idcategoria, idespec)
+FROM '/docker-entrypoint-initdb.d/initial_data/DBCC(categoria_especificacion).csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY componente(idcomponente, idcategoria, marca,	nombre,	descripcion, url_imagen)
+FROM '/docker-entrypoint-initdb.d/initial_data/DBCC(componente).csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY inventario(idcomponente,	SKU,	disponibilidad,	precio)
+FROM '/docker-entrypoint-initdb.d/initial_data/DBCC(inventario).csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY componente_espec(idcat_espec,idcomponente,valor)
+FROM '/docker-entrypoint-initdb.d/initial_data/DBCC(componente_especificacion).csv'
+DELIMITER ','
+CSV HEADER;
