c66b14f49a3026a99e6168098ee2659b0a17b210
CarlosDRomero <cdavid.romeror@gmail.com>
2024-11-08 07:57:29 -0500

    Add next condition to 'notificar'

--- controllers/notificacion.js
+++ controllers/notificacion.js
@@ -2,17 +2,21 @@ import { notificacionModel } from "../models/notificacion.js";
 import { notificacionService } from "../services/notificaciones.js"
 
 export const notificacionController = {
-  notificar: async (req) => {
-    req.payload.forEach(async payload => {
-      const { notificacion, email, objetivo } = payload
-      if (!notificacion) return;
-      console.log(notificacion)
-      const notificaciondb = await notificacionService.crearNotificacion(notificacion)
-      notificacionService.notificarSockets(objetivo, notificaciondb)
-      if (email && notificaciondb){
-        notificacionService.notificarACorreo(email,notificaciondb,notificacion.anexos )
-      }
-    });
+  notificar: async (req, _, next) => {
+    if (req.payload){
+
+      req.payload.forEach(async payload => {
+        const { notificacion, email, objetivo } = payload
+        if (!notificacion) return;
+        console.log(notificacion)
+        const notificaciondb = await notificacionService.crearNotificacion(notificacion)
+        notificacionService.notificarSockets(objetivo, notificaciondb)
+        if (email && notificaciondb){
+          notificacionService.notificarACorreo(email,notificaciondb,notificacion.anexos )
+        }
+      });
+    }
+    next()
   },
   obtenerNotificacionesUsuario: async (req, res) => {
     const { idusuario } = req.usuario
